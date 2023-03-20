5b27af0b027b0e44ceed4c834ce57714c5a9baba
adisch <andy12@gmx.ch>
2023-03-20 15:37:58 +0100

    table information query added

--- query_datapool.py
+++ query_datapool.py
@@ -4,11 +4,11 @@ import pendulum
 
 from datapool_client import DataPool
 
-target_directory = Path("C:/Users/dischand/switchdrive/UWO/Arbeiten und Artikel/UWO_Data_paper/_ERIC/UWO_datapaper_ERIC/_A_field_observations")
+target_directory = Path("C:/Users/dischand/VisualStudioProjects/data/processed")
 
 datapool_instance = DataPool(to_replace={"parameter": "variable"})
 
-sources = ["bn_r03_rub_morg", "bf_plsZUL1100_inflow_ara"]
+sources = ["bn_r02_school_chatzenrainstr"]
 
 start = "2016-01-01 00:00:00"
 end = pendulum.now("Europe/Zurich").to_datetime_string()
